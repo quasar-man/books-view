b9e9d9eddd8c3fa500fe6fb554b8261151e30cf9
quasar-man <yu8u7engine@gmail.com>
2021-06-13 23:19:38 +0900

    管理ツールのエラーページを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  def manager_render_404
+    render template: "manager/errors/error_404", status: 404, layout: "manager", context_type: "text/html"
+  end
+
+  def manager_render_500
+    render template: "manager/errors/error_500", status: 500, layout: "manager", context_type: "text/html"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   namespace :manager do
     resources :categories
   end
+
+  get "*path", controller: "application", action: "manager_render_404"
+  get "*path", controller: "application", action: "manager_render_500"
 end
