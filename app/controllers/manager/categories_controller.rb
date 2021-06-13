2802ee86072d8d3cfbf07de9166f103fe24c0e6f
quasar-man <yu8u7engine@gmail.com>
2021-06-13 23:25:55 +0900

    Create book's category manager.

--- app/controllers/manager/categories_controller.rb
+++ app/controllers/manager/categories_controller.rb
@@ -1,4 +1,47 @@
 class Manager::CategoriesController < ApplicationController
+  layout 'manager'
+
+  rescue_from Exception,                    with: :manager_render_500
+  rescue_from ActiveRecord::RecordNotFound, with: :manager_render_404
+  rescue_from ActionController::RoutingError, with: :manager_render_404
+
   def index
+    @categories = Category.all
   end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    category = Category.new(category_params)
+    if category.save
+      redirect_to action: "index"
+    else
+      redirect_to action: "new"
+    end
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    category = Category.find(params[:id])
+    category.update(category_params)
+
+    redirect_to controller: :categories, action: :index
+  end
+
+  def destroy
+    Category.find(params[:id])&.destroy
+
+    redirect_to controller: :categories, action: :index
+  end
+    
+  private
+
+    def category_params
+      params.require(:category).permit(:name)
+    end
 end
